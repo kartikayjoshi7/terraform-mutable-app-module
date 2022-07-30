10b83c647c58ed9e14b0da6ea5cae28e0e4b2f75
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 05:18:30 +0530

    File updated

--- route53.tf
+++ route53.tf
@@ -1,8 +1,17 @@
 resource "aws_route53_record" "private-record" {
   count = var.LB_PRIVATE ? 1 : 0
   zone_id = data.terraform_remote_state.vpc.id.PRIVATE_HOSTED_ZONE_ID
-  name    = "www.example.com"
+  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
   type    = "A"
   ttl     = 300
-  records = [aws_eip.lb.public_ip]
+  records = [data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME]
 }
+
+#resource "aws_route53_record" "public-record" {
+#  count = var.LB_PUBLIC ? 1 : 0
+#  zone_id = data.terraform_remote_state.vpc.id.PRIVATE_HOSTED_ZONE_ID
+#  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
+#  type    = "A"
+#  ttl     = 300
+#  records = [data.terraform_remote_state.vpc.outputs.PRIVATE_LB_DNSNAME]
+#}
