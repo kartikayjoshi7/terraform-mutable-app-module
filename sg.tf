98739cfbc0e8ad135fd6c9d5121db62c1909f9e5
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 03:54:05 +0530

    File updated

---
+++ sg.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "sg" {
+  name        = "sg_${var.COMPONENT}_${var.ENV}"
+  description = "sg_${var.COMPONENT}_${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "APP"
+    from_port        = var.APP_PORT
+    to_port          = var.APP_PORT
+    protocol         = "tcp"
+    cidr_blocks      = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR
+
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = concat(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR, tolist([data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]))
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg_${var.COMPONENT}_${var.ENV}"
+  }
+}
+
+
