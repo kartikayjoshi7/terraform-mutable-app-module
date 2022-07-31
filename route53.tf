aa1e1d7a00d2577be63818c13ea4fce252341adf
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 05:45:21 +0530

    File updated

--- route53.tf
+++ route53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "private-record" {
   count = var.LB_PRIVATE ? 1 : 0
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME]
 }
